de06e1c1758adc739f93bedf6a595a02858cea61
JasonL24 <32722492+JasonL24@users.noreply.github.com>
2020-09-05 22:05:30 -0700

    Updates

---
+++ precache-manifest.2eb9d08014fa725325dac127cca98bae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00564f79e881b4ffcb6fe28da78a580d",
+    "url": "/mindfull/index.html"
+  },
+  {
+    "revision": "0e61548d1836070d713a",
+    "url": "/mindfull/static/css/main.c7d5cae4.chunk.css"
+  },
+  {
+    "revision": "95fce35090441c1f6bb8",
+    "url": "/mindfull/static/js/2.121332fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mindfull/static/js/2.121332fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e61548d1836070d713a",
+    "url": "/mindfull/static/js/main.5e5bf7b7.chunk.js"
+  },
+  {
+    "revision": "fdbafa5cdb320414eb82",
+    "url": "/mindfull/static/js/runtime-main.f9c2c775.js"
+  },
+  {
+    "revision": "9d5ade2aedde81c0eb0ab7cb32c38665",
+    "url": "/mindfull/static/media/Homepage.9d5ade2a.png"
+  }
+]);
