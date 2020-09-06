553a356deca42a3220e67615d7c93148c9420059
JasonL24 <32722492+JasonL24@users.noreply.github.com>
2020-09-05 20:27:57 -0700

    styling updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
-      <div>
-          <h1 className="heading">Active Mind</h1>
+      <div className="head-container">
+          <h1 className="heading">MindFull</h1>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
       </div>
     )
 }
